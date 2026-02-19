ddd7bcb1430f2b733bd892759d308d0bb8316e1d
timmy-turner005 <timitalented005@gmail.com>
2026-02-19 14:28:43 +0100

    first commit

---
+++ app/components/subscribers.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function subscribers() {
+  return (
+    <div>
+      <Button variant="destructive">Button</Button>
+    </div>
+  );
+}
