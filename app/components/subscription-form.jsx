89a2c58e1ce2690337efeee7f53b3a777c1575fd
timmy-turner005 <timitalented005@gmail.com>
2026-02-23 16:19:51 +0100

    subscription form done

--- app/components/subscription-form.jsx
+++ app/components/subscription-form.jsx
@@ -1,5 +1,99 @@
+"use client";
+
 import React from "react";
 
-export default function subscriptionForm() {
-  return <div>Subscription Form</div>;
-}
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+
+// import { toast } from "sonner";
+
+const formSchema = z.object({
+  fullName: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters" }),
+  email: z.string().email({ message: "Invalid email address" }),
+});
+
+export default function SubscriptionForm() {
+  // 1. Define your form.
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values) {
+    console.log(values);
+    toast.success(`${values.fullName} subscribed successfully`);
+  }
+
+  // 3. Get the status
+  const { isSubmitting, isValid } = form.formState;
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-4 my-4 flex flex-col items-center"
+      >
+        <FormField
+          control={form.control}
+          name="fullName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel></FormLabel>
+              <FormControl>
+                <Input
+                  className="w-[350px]"
+                  disabled={isSubmitting}
+                  placeholder="Enter name"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel></FormLabel>
+              <FormControl>
+                <Input
+                  className="w-[350px]"
+                  disabled={isSubmitting}
+                  type="email"
+                  placeholder="Enter e-mail"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button disabled={!isValid || isSubmitting} type="submit">
+          Subscribe
+        </Button>
+      </form>
+    </Form>
+  );
+};
